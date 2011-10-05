14f187fe03d79e4c210a8c35648d99de7b7476f6
Joshua Burkhart <joshuagburkhart@gmail.com>
2011-10-04 21:50:40 -0400

    installed rails

---
+++ source/rb/www/riviere-rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RiviereRails::Application.config.secret_token = '03226e1a8ced697c16ad21b04c4baa10616118bd2fdc2cc073629d3c96752d3839420ae214d000b2eb47dacdfa5bc11000b9d3c17cf1f82e563ec2256fcde35c'
