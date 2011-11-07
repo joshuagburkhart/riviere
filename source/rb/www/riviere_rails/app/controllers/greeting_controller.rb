2cd55c9826bd4d22ce7623100bee489a1fdcc323
Joshua Burkhart <joshuagburkhart@gmail.com>
2011-11-06 22:33:00 -0500

    deleted some old files

--- source/rb/www/riviere_rails/app/controllers/greeting_controller.rb
+++
@@ -1,8 +0,0 @@
-class GreetingController < ApplicationController
-  def index
-  	@age=8
-	@table={"headings" => ["addend", "addend", "sum"],
-		"body"	   => [[1,1,2],[1,2,3],[1,3,4]]
-		}
-  end
-end
